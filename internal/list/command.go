89e425a62d64a2fd626dfc6af8a1e18f878f9dc2
amd <amd@gurge.com>
2026-05-08 20:28:18 -0700

    wip

--- internal/list/command.go
+++ internal/list/command.go
@@ -3,11 +3,14 @@ package list
 import (
 	"context"
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/gurgeous/gshoot/internal/auth"
 	"github.com/gurgeous/gshoot/internal/google"
-	"github.com/gurgeous/gshoot/internal/ux"
+	"github.com/schollz/progressbar/v3"
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 	"google.golang.org/api/drive/v3"
 )
 
@@ -57,7 +60,45 @@ func run(cmd *cobra.Command, _ []string) error {
 	}
 
 	// list
-	stopDots := ux.StartDots(cmd.ErrOrStderr(), "listing spreadsheets...")
+	stopDots := func(string) {}
+	file, ok := cmd.ErrOrStderr().(*os.File)
+	fd := uintptr(0)
+	if ok {
+		fd = file.Fd()
+	}
+	if ok && fd <= uintptr(^uint(0)>>1) && term.IsTerminal(int(fd)) {
+		bar := progressbar.NewOptions(-1,
+			progressbar.OptionSetWriter(file),
+			progressbar.OptionSetDescription("listing spreadsheets..."),
+			progressbar.OptionSpinnerType(14),
+			progressbar.OptionThrottle(50*time.Millisecond),
+			progressbar.OptionClearOnFinish(),
+		)
+
+		stop := make(chan string, 1)
+		stopped := make(chan struct{})
+		go func() {
+			ticker := time.NewTicker(50 * time.Millisecond)
+			defer ticker.Stop()
+			defer close(stopped)
+
+			for {
+				select {
+				case <-ticker.C:
+					_ = bar.Add(1)
+				case msg := <-stop:
+					_ = bar.Finish()
+					fmt.Fprintf(file, "✓ %s\n", msg)
+					return
+				}
+			}
+		}()
+
+		stopDots = func(msg string) {
+			stop <- msg
+			<-stopped
+		}
+	}
 	files, err := listRecent(ctx, client, 10)
 	if err != nil {
 		stopDots("list failed")
