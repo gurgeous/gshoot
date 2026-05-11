3671e6ac7c4e52c4cf00439244a7b10e8b6472c1
amd <amd@gurge.com>
2026-05-10 22:42:40 -0700

    wip

--- internal/sub/down_test.go
+++ internal/sub/down_test.go
@@ -13,28 +13,7 @@ import (
 
 func TestDownCommand(t *testing.T) {
 	withRawTokenAuth(t)
-	withAPI(t, newDownAPIHandler(t))
-
-	// => stdout
-	_, stdout, _ := testMain("down", "Budget")
-	assert.Equal(t, "name,count\nalpha,1\n", stdout)
-
-	// => file
-	path := filepath.Join(t.TempDir(), "out.csv")
-	_, stdout, _ = testMain("down", "Budget", "--output", path)
-	data, _ := os.ReadFile(path)
-	assert.Equal(t, "", stdout)
-	assert.Equal(t, "name,count\nalpha,1\n", string(data))
-}
-
-//
-// fake google api
-//
-
-func newDownAPIHandler(t *testing.T) http.HandlerFunc {
-	t.Helper()
-
-	return func(w http.ResponseWriter, r *http.Request) {
+	withAPI(t, func(w http.ResponseWriter, r *http.Request) {
 		switch {
 		case r.URL.Path == "/drive/v3/files":
 			_ = json.NewEncoder(w).Encode(map[string]any{
@@ -55,5 +34,16 @@ func newDownAPIHandler(t *testing.T) http.HandlerFunc {
 		default:
 			t.Fatalf("unexpected path: %s", r.URL.Path)
 		}
-	}
+	})
+
+	// => stdout
+	_, stdout, _ := testMain("down", "Budget")
+	assert.Equal(t, "name,count\nalpha,1\n", stdout)
+
+	// => file
+	path := filepath.Join(t.TempDir(), "out.csv")
+	_, stdout, _ = testMain("down", "Budget", "--output", path)
+	data, _ := os.ReadFile(path)
+	assert.Equal(t, "", stdout)
+	assert.Equal(t, "name,count\nalpha,1\n", string(data))
 }
