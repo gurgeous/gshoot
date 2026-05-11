4d2afc386a8f467fe62ac7832422f06cf65fd5e0
amd <amd@gurge.com>
2026-05-10 20:22:42 -0700

    wip

--- internal/sub/sub_helpers_test.go
+++ internal/sub/sub_helpers_test.go
@@ -62,6 +62,16 @@ func (fn roundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 }
 
 //
+// wee helper that calls Main and captures output
+//
+
+func testMain(args ...string) (int, string, string) {
+	var stdout, stderr bytes.Buffer
+	code := Main(args, &stdout, &stderr)
+	return code, stdout.String(), stderr.String()
+}
+
+//
 // env mocks
 //
 
@@ -98,11 +108,4 @@ func tTempDir(t testutil.TestingT) string {
 		t.Fatalf("test helper needs TempDir")
 	}
 	return tt.TempDir()
-}
-
-func testMain(args ...string) (int, string, string) {
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	code := Main(args, &stdout, &stderr)
-	return code, stdout.String(), stderr.String()
 }
